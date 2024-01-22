97c4881ef529e02ed592479598b76774b9df8396
= <=>
2024-01-22 08:48:11 +0530

    configure=[auth-layout]

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db"
+import { redirect } from "next/navigation"
+
+interface RootLayoutProps {
+      children: React.ReactNode
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
+      const { userId } = auth();
+
+      if (!userId) {
+            redirect("/sign-in")
+      }
+
+      const dbStore = await db.store.findFirst({
+            where: {
+                  userId
+            }
+      });
+
+      if (dbStore) {
+            redirect(`/${dbStore.id}`)
+      }
+
+      return <div className="min-h-screen flex items-center justify-center">{children}</div>
+}
